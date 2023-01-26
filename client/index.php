e342be2e0acf6bbadf40d6c3cda76fe3f833c385
Dang Huy Hoang <doiquanhieumk@gmail.com>
2023-01-27 00:14:31 +0700

    hotfix

--- client/index.php
+++ client/index.php
@@ -26,9 +26,12 @@
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="myFavor.php">My Favorite Places</a></li>
                 </ul>
-                <form class="d-flex">
+                <form class="nav-bar-nav ms-auto">
                     <button class="btn btn-outline-dark" type="submit">
-                        Login
+                        Sign In
+                    </button>
+                    <button class="btn btn-outline-dark" type="submit">
+                        Sign Out
                     </button>
                 </form>
             </div>
@@ -47,17 +50,15 @@
     <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                <div class="col mb-5">
+            <div class="col mb-5">
                     <div class="card h-100">
-                        <!-- Product image-->
+                        <!-- Place image-->
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
+                        <!-- Place details-->
                         <div class="card-body p-4">
                             <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Fancy Product</h5>
-                                <!-- Product price-->
-                                $40.00 - $80.00
+                                <!-- Place name-->
+                                <h5 class="fw-bolder">Fancy Place</h5>
                             </div>
                         </div>
                         <!-- Product actions-->
@@ -68,26 +69,13 @@
                 </div>
                 <div class="col mb-5">
                     <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                        <!-- Product image-->
+                        <!-- Place image-->
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
+                        <!-- Place details-->
                         <div class="card-body p-4">
                             <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Special Item</h5>
-                                <!-- Product reviews-->
-                                <div class="d-flex justify-content-center small text-warning mb-2">
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                </div>
-                                <!-- Product price-->
-                                <span class="text-muted text-decoration-line-through">$20.00</span>
-                                $18.00
+                                <!-- Place name-->
+                                <h5 class="fw-bolder">Fancy Place</h5>
                             </div>
                         </div>
                         <!-- Product actions-->
@@ -98,18 +86,13 @@
                 </div>
                 <div class="col mb-5">
                     <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                        <!-- Product image-->
+                        <!-- Place image-->
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
+                        <!-- Place details-->
                         <div class="card-body p-4">
                             <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Sale Item</h5>
-                                <!-- Product price-->
-                                <span class="text-muted text-decoration-line-through">$50.00</span>
-                                $25.00
+                                <!-- Place name-->
+                                <h5 class="fw-bolder">Fancy Place</h5>
                             </div>
                         </div>
                         <!-- Product actions-->
@@ -120,23 +103,13 @@
                 </div>
                 <div class="col mb-5">
                     <div class="card h-100">
-                        <!-- Product image-->
+                        <!-- Place image-->
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
+                        <!-- Place details-->
                         <div class="card-body p-4">
                             <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Popular Item</h5>
-                                <!-- Product reviews-->
-                                <div class="d-flex justify-content-center small text-warning mb-2">
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                </div>
-                                <!-- Product price-->
-                                $40.00
+                                <!-- Place name-->
+                                <h5 class="fw-bolder">Fancy Place</h5>
                             </div>
                         </div>
                         <!-- Product actions-->
@@ -147,18 +120,13 @@
                 </div>
                 <div class="col mb-5">
                     <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                        <!-- Product image-->
+                        <!-- Place image-->
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
+                        <!-- Place details-->
                         <div class="card-body p-4">
                             <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Sale Item</h5>
-                                <!-- Product price-->
-                                <span class="text-muted text-decoration-line-through">$50.00</span>
-                                $25.00
+                                <!-- Place name-->
+                                <h5 class="fw-bolder">Fancy Place</h5>
                             </div>
                         </div>
                         <!-- Product actions-->
@@ -169,15 +137,13 @@
                 </div>
                 <div class="col mb-5">
                     <div class="card h-100">
-                        <!-- Product image-->
+                        <!-- Place image-->
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
+                        <!-- Place details-->
                         <div class="card-body p-4">
                             <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Fancy Product</h5>
-                                <!-- Product price-->
-                                $120.00 - $280.00
+                                <!-- Place name-->
+                                <h5 class="fw-bolder">Fancy Place</h5>
                             </div>
                         </div>
                         <!-- Product actions-->
@@ -188,26 +154,13 @@
                 </div>
                 <div class="col mb-5">
                     <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                        <!-- Product image-->
+                        <!-- Place image-->
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
+                        <!-- Place details-->
                         <div class="card-body p-4">
                             <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Special Item</h5>
-                                <!-- Product reviews-->
-                                <div class="d-flex justify-content-center small text-warning mb-2">
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                </div>
-                                <!-- Product price-->
-                                <span class="text-muted text-decoration-line-through">$20.00</span>
-                                $18.00
+                                <!-- Place name-->
+                                <h5 class="fw-bolder">Fancy Place</h5>
                             </div>
                         </div>
                         <!-- Product actions-->
@@ -218,23 +171,13 @@
                 </div>
                 <div class="col mb-5">
                     <div class="card h-100">
-                        <!-- Product image-->
+                        <!-- Place image-->
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
+                        <!-- Place details-->
                         <div class="card-body p-4">
                             <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Popular Item</h5>
-                                <!-- Product reviews-->
-                                <div class="d-flex justify-content-center small text-warning mb-2">
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                </div>
-                                <!-- Product price-->
-                                $40.00
+                                <!-- Place name-->
+                                <h5 class="fw-bolder">Fancy Place</h5>
                             </div>
                         </div>
                         <!-- Product actions-->
