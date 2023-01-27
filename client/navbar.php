2aba7852f9f2cfb0a7180e4f57e2c1446e97a57c
AnhNguyen208 <nguyenletuananh20082001@gmail.com>
2023-01-27 21:20:09 +0700

    update index

--- client/navbar.php
+++ client/navbar.php
@@ -6,7 +6,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
-                <!-- <li class="nav-item"><a class="nav-link" href="myFavor.php">My Favorite Places</a></li> -->
+                <li class="nav-item"><a class="nav-link" href="myFavor.php">My Favorite Places</a></li>
             </ul>
             <form class="nav-bar-nav ms-auto" method="post">
                 <?php
